90fc741865e825fdefcf298db019f5d354916769
scrawl <88452486@qq.com>
2018-05-02 17:45:11 +0800

    test commit 1

---
+++ test-socket/src/main/java/com/as/test/socket/Client.java
@@ -0,0 +1,13 @@
+package com.as.test.socket;
+
+import java.net.Socket;
+
+/**
+ * Desc:
+ * Create by scrawl on 2018/5/2
+ */
+public class Client {
+    public static void main(String[] args) {
+//        Socket
+    }
+}
---
+++ test-socket/src/main/java/com/as/test/socket/Server.java
@@ -0,0 +1,8 @@
+package com.as.test.socket;
+
+/**
+ * Desc:
+ * Create by scrawl on 2018/5/2
+ */
+public class Server {
+}
