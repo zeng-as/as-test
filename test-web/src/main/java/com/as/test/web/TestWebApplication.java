a1ca28d5a5962ca3b2b7c333a013bc91208f5474
as <88452486@qq.com>
2018-09-01 19:03:26 +0800

    好久没提交了，提交一波

--- test-web/src/main/java/com/as/test/web/TestWebApplication.java
+++ test-web/src/main/java/com/as/test/web/TestWebApplication.java
@@ -1,13 +1,12 @@
 package com.as.test.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Desc:
  * Create by scrawl on 2018/5/5
  */
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class TestWebApplication {
     public static void main(String[] args) {
