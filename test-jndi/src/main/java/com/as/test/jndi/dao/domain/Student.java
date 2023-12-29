327fe1c57287f3e15b70deaa17b4627ea0aa4f14
as <zengsq@meyacom.com>
2023-12-29 16:38:03 +0800

    just commit

---
+++ test-jndi/src/main/java/com/as/test/jndi/dao/domain/Student.java
@@ -0,0 +1,55 @@
+package com.as.test.jndi.dao.domain;
+
+import java.util.Date;
+
+public class Student {
+    private String id;
+
+    private String name;
+
+    private Byte sex;
+
+    private Integer age;
+
+    private Date createTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Byte getSex() {
+        return sex;
+    }
+
+    public void setSex(Byte sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ test-jndi/src/main/java/com/as/test/jndi/dao/mapper/StudentMapper.java
@@ -0,0 +1,17 @@
+package com.as.test.jndi.dao.mapper;
+
+import com.as.test.jndi.dao.domain.Student;
+
+public interface StudentMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(Student record);
+
+    int insertSelective(Student record);
+
+    Student selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(Student record);
+
+    int updateByPrimaryKey(Student record);
+}
